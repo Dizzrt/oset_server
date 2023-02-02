cc6ef09e31af59efffa5fd147e4e366df578bd76
Dizzrt <dizzrt.s@qq.com>
2023-02-02 21:11:34 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module oset_server
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,14 @@
+//
+// File: main.go
+// Created by Dizzrt on 2023/02/02.
+//
+// Copyright (C) 2023 The oset Authors.
+// This source code is licensed under the MIT license found in
+// the LICENSE file in the root directory of this source tree.
+//
+
+package main
+
+func main() {
+
+}
